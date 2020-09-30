6725f5d46c692e272542a6b85c671d3cd5eb70e2
Hadi Halim <pr4.playbook88@gmail.com>
2020-10-01 00:44:05 +0700

    feat: static trap

--- packages/base/__tests__/base.test.js
+++ packages/base/__tests__/base.test.js
@@ -1,7 +1,7 @@
 const { expect } = require('chai');
 const { get } = require('http');
 const { it } = require('mocha');
-const { MagicalObject } = require('./../lib/MagicalObject');
+const { MagicalObject, MyClass } = require('./../lib/MagicalObject');
 describe('no extended class', function () {
     describe('test setter and getter property', function () {
         class testMagicalObject extends MagicalObject {
@@ -210,6 +210,249 @@ describe('With extended class', function () {
                 //console.log(error.message);
                 expect(error.name).equal('Exception');
                 expect(error.message).equal('Undefined property: testMagicalObject.test2');
+            }
+        });
+    });
+});
+
+describe('static module no extended class', function () {
+    describe('test setter and getter property', function () {
+        class testMagicalObject extends MagicalObject {
+            static test = 'value test';
+            static noValue;
+        }
+        testMagicalObject = testMagicalObject.proxyStatic();
+        it('should error trying trap __invoke', function () {
+            try {
+                testMagicalObject('test');
+            } catch (error) {
+                //console.log(error.message);
+                expect(error.name).equal('Exception');
+                expect(error.message).equal('Undefined static property: testMagicalObject.__invoke');
+            }
+        });
+
+        it('should correct for valid get', function () {
+            expect(testMagicalObject.test).equal('value test');
+        });
+        it('should correct for valid get undefined variable', function () {
+            expect(testMagicalObject.noValue).equal(undefined);
+        });
+        it('should error for get undeclared property', function () {
+            try {
+                testMagicalObject.test2;
+            } catch (error) {
+                //console.log(error.message);
+                expect(error.name).equal('Exception');
+                expect(error.message).equal('Undefined static property: testMagicalObject.test2');
+            }
+        });
+        it('should correct for valid set', function () {
+            testMagicalObject.test = 'changed value test';
+            expect(testMagicalObject.test).equal('changed value test');
+        });
+        it('should correct for valid set undefined', function () {
+            testMagicalObject.noValue = 'changed value defined';
+            expect(testMagicalObject.noValue).equal('changed value defined');
+        });
+        it('should allow to set undeclared property', function () {
+            try {
+                testMagicalObject.test2 = 'new value test2';
+            } catch (error) {
+                //console.log(error.message);
+                expect(error.name).equal('Exception');
+                expect(error.message).equal('Undefined static property: testMagicalObject.test2');
+            }
+        });
+    });
+    describe('test isset property', function () {
+        class testMagicalObject extends MagicalObject {
+            static test = 'value test';
+            static noValue;
+        }
+        testMagicalObject = testMagicalObject.proxyStatic();
+        describe('test with "in"', function () {
+            it('should true for variable has value"', function () {
+                expect('test' in testMagicalObject).equal(true);
+            });
+            it('should true for variable has no value"', function () {
+                expect('noValue' in testMagicalObject).equal(true);
+            });
+            it('should false for undeclared variable', function () {
+                expect('justtest' in testMagicalObject).equal(false);
+            });
+        });
+        describe('test with "hasOwnProperty"', function () {
+            it('should can variable has value ', function () {
+                expect(testMagicalObject.hasOwnProperty('test')).equal(true);
+            });
+            it('should true for variable has not value"', function () {
+                expect(testMagicalObject.hasOwnProperty('noValue')).equal(true);
+            });
+            it('should false for undeclared variable"', function () {
+                expect(testMagicalObject.hasOwnProperty('justtest')).equal(false);
+            });
+        });
+    });
+    describe('test unset property', function () {
+        class testMagicalObject extends MagicalObject {
+            static test = 'value test';
+            static noValue;
+        }
+        testMagicalObject = testMagicalObject.proxyStatic();
+        it('should can delete variable has value ', function () {
+            delete testMagicalObject.test;
+            expect('test' in testMagicalObject).equal(false);
+            expect(testMagicalObject.hasOwnProperty('test')).equal(false);
+        });
+        it('should can delete variable has not value ', function () {
+            delete testMagicalObject.noValue;
+            expect('noValue' in testMagicalObject).equal(false);
+            expect(testMagicalObject.hasOwnProperty('noValue')).equal(false);
+        });
+        it('should can delete undeclared variable ', function () {
+            delete testMagicalObject.test2;
+            expect('test2' in testMagicalObject).equal(false);
+            expect(testMagicalObject.hasOwnProperty('test2')).equal(false);
+        });
+    });
+});
+describe('static module With extended class', function () {
+    describe('test setter and getter property', function () {
+        class testMagicalObject extends MagicalObject {
+            static test = 'value test';
+            static noValue;
+            static __get(props) {
+                var val = super.__get(props);
+                return val + '2';
+            }
+            static __set(props, value) {
+                return super.__set(props, value + '3');
+            }
+            static __invoke(...args) {
+                return args;
+            }
+        }
+        testMagicalObject = testMagicalObject.proxyStatic();
+        it('should valid trying trap __invoke', function () {
+            const results = testMagicalObject('test', 'test2');
+            expect(results.length).equal(2);
+            expect(results[0]).equal('test');
+            expect(results[1]).equal('test2');
+        });
+
+        it('should correct for valid get', function () {
+            expect(testMagicalObject.test).equal('value test2');
+        });
+        it('should correct for valid get undefined variable', function () {
+            expect(testMagicalObject.noValue).equal('undefined2');
+        });
+        it('should error for get undeclared property', function () {
+            try {
+                testMagicalObject.test2;
+            } catch (error) {
+                //console.log(error.message);
+                expect(error.name).equal('Exception');
+                expect(error.message).equal('Undefined static property: testMagicalObject.test2');
+            }
+        });
+        it('should correct for valid set', function () {
+            testMagicalObject.test = 'changed value test';
+            expect(testMagicalObject.test).equal('changed value test32');
+        });
+        it('should correct for valid set undefined', function () {
+            testMagicalObject.noValue = 'changed value defined';
+            expect(testMagicalObject.noValue).equal('changed value defined32');
+        });
+        it('should allow to set undeclared property', function () {
+            try {
+                testMagicalObject.test2 = 'new value test';
+            } catch (error) {
+                //console.log(error.message);
+                expect(error.name).equal('Exception');
+                expect(error.message).equal('Undefined static property: testMagicalObject.test2');
+            }
+        });
+    });
+    describe('test isset property', function () {
+        class testMagicalObject extends MagicalObject {
+            static test = 'value test';
+            static noValue;
+            static __isset(prop) {
+                return !super.__isset(prop);
+            }
+        }
+        testMagicalObject = testMagicalObject.proxyStatic();
+        describe('test with "in"', function () {
+            it('should true for variable has value"', function () {
+                expect('test' in testMagicalObject).equal(false);
+            });
+            it('should true for variable has no value"', function () {
+                expect('noValue' in testMagicalObject).equal(false);
+            });
+            it('should false for undeclared variable', function () {
+                expect('justtest' in testMagicalObject).equal(true);
+            });
+        });
+        describe('test with "hasOwnProperty"', function () {
+            it('should can variable has value ', function () {
+                expect(testMagicalObject.hasOwnProperty('test')).equal(false);
+            });
+            it('should true for variable has not value"', function () {
+                expect(testMagicalObject.hasOwnProperty('noValue')).equal(false);
+            });
+            it('should false for undeclared variable"', function () {
+                expect(testMagicalObject.hasOwnProperty('justtest')).equal(true);
+            });
+        });
+    });
+    describe('test unset property', function () {
+        class testMagicalObject extends MagicalObject {
+            static test = 'value test';
+            static noValue;
+            static __unset(prop) {
+                this['new_' + prop] = this[prop];
+                return super.__unset(prop);
+            }
+        }
+        testMagicalObject = testMagicalObject.proxyStatic();
+        it('should can delete variable has value ', function () {
+            delete testMagicalObject.test;
+            expect('test' in testMagicalObject).equal(false);
+            expect(testMagicalObject.hasOwnProperty('test')).equal(false);
+            expect(testMagicalObject.new_test).equal('value test');
+            try {
+                testMagicalObject.test;
+            } catch (error) {
+                //console.log(error.message);
+                expect(error.name).equal('Exception');
+                expect(error.message).equal('Undefined static property: testMagicalObject.test');
+            }
+        });
+        it('should can delete variable has not value ', function () {
+            delete testMagicalObject.noValue;
+            expect('noValue' in testMagicalObject).equal(false);
+            expect(testMagicalObject.hasOwnProperty('noValue')).equal(false);
+            expect(testMagicalObject.new_noValue).equal(undefined);
+            try {
+                testMagicalObject.noValue;
+            } catch (error) {
+                //console.log(error.message);
+                expect(error.name).equal('Exception');
+                expect(error.message).equal('Undefined static property: testMagicalObject.noValue');
+            }
+        });
+        it('should can delete undeclared variable ', function () {
+            delete testMagicalObject.test2;
+            expect('test2' in testMagicalObject).equal(false);
+            expect(testMagicalObject.hasOwnProperty('test2')).equal(false);
+            expect(testMagicalObject.new_test2).equal(undefined);
+            try {
+                testMagicalObject.test2;
+            } catch (error) {
+                //console.log(error.message);
+                expect(error.name).equal('Exception');
+                expect(error.message).equal('Undefined static property: testMagicalObject.test2');
             }
         });
     });
--- packages/base/src/MagicalObject.ts
+++ packages/base/src/MagicalObject.ts
@@ -2,10 +2,10 @@
 import Exception from './Exception';
 /*
 PHP magic method
-// __construct(), 
+// __construct()**, 
 //__destruct(), 
-//__call(), 
-//__callStatic(), 
+//__call(), **
+//__callStatic() **, 
 //__get(), **
 //__set(), **
 //__isset(), **
@@ -15,7 +15,7 @@ PHP magic method
 // __serialize(), 
 //__unserialize(), 
 // __toString(), ** 
-// __invoke(), 
+// __invoke()*, 
 // __set_state(),
 // __clone() 
 // __debugInfo()
@@ -25,16 +25,77 @@ additional magic method
 */
 
 export type MagicalObjectProp = string | number | symbol;
-class MagicalObjectClass {
+export class MagicalObject {
+    static proxyStatic(): void {
+        const proxied = new Proxy(this, {
+            apply: function (obj: any, thisArg: any, argumentsList: any): any {
+                if (!obj.hasOwnProperty('__invoke') || typeof obj['__invoke'] !== 'function') {
+                    throw new Exception('Undefined static property: ' + obj.name + '.' + '__invoke');
+                }
+                return obj.__invoke(...argumentsList);
+            },
+            get: (obj: any, prop: PropertyKey, receiver?: any): any => {
+                if (prop === 'hasOwnProperty' && obj[prop] != undefined && typeof obj[prop] === 'function') {
+                    return function (...args) {
+                        return obj.__isset(args);
+                    };
+                } else if (obj[prop] != undefined && typeof obj[prop] == 'function') {
+                    // Wrap it around a function and return it
+                    if (prop.toString() === '__get') {
+                        return function (...args) {
+                            return obj.__get.bind(obj)(args);
+                        };
+                    } else {
+                        return function (...args) {
+                            return obj.__call.bind(obj)(prop, args);
+                        };
+                    }
+                } else {
+                    if (!obj.hasOwnProperty(prop)) {
+                        throw new Exception('Undefined static property: ' + obj.name + '.' + (prop as string));
+                    }
+
+                    return obj.__get(prop, receiver);
+                }
+            },
+            set: (obj: any, prop: PropertyKey, value: any, receiver: any): boolean => {
+                if (!receiver.hasOwnProperty(prop)) {
+                    throw new Exception('Undefined static property: ' + obj.name + '.' + (prop as string));
+                }
+                return obj.__set(prop, value);
+            },
+            has: (obj: any, prop: PropertyKey): boolean => {
+                return obj.__isset(prop);
+            },
+            deleteProperty: (obj: any, prop: PropertyKey): boolean => {
+                return obj.__unset(prop);
+            },
+        });
+        return proxied;
+    }
+    static __get(prop: PropertyKey): unknown {
+        return this[prop];
+    }
     __get(prop: PropertyKey): unknown {
         return this[prop];
+    }
+    static __set(prop: PropertyKey, value: unknown): boolean {
+        this[prop] = value;
+        return true;
     }
     __set(prop: PropertyKey, value: unknown): boolean {
         this[prop] = value;
         return true;
     }
+    static __isset(prop: PropertyKey): boolean {
+        return prop in this && this.hasOwnProperty(prop);
+    }
     __isset(prop: PropertyKey): boolean {
         return prop in this && this.hasOwnProperty(prop);
+    }
+    static __unset(prop: PropertyKey): boolean {
+        delete this[prop];
+        return true;
     }
     __unset(prop: PropertyKey): boolean {
         delete this[prop];
@@ -43,18 +104,22 @@ class MagicalObjectClass {
     toString(): string {
         return this.__toString();
     }
+    static toString(): string {
+        return this.__toString();
+    }
     __toString(): string {
+        return this.constructor.name;
+    }
+    static __toString(): string {
         return this.constructor.name;
     }
     __call(prop: PropertyKey, args: Array<any>): any {
         return this[prop](...args);
     }
+    static __call(prop: PropertyKey, args: Array<any>): any {
+        return this[prop](...args);
+    }
     constructor() {
-        /*
-        if (getParams(this.__get).length !== 1) {
-            throw new InvalidMagicObject('__set need 1 params');
-        }
-        */
         return new Proxy(this, {
             get: (obj: any, prop: PropertyKey, receiver?: any): any => {
                 if (prop === 'hasOwnProperty' && obj[prop] != undefined && typeof obj[prop] == 'function') {
@@ -82,12 +147,12 @@ class MagicalObjectClass {
             },
             set: (obj: any, prop: PropertyKey, value: any): boolean => {
                 if (!obj.hasOwnProperty(prop)) {
-                    Object.defineProperty(obj, 'property1', { writable: true });
+                    Object.defineProperty(obj, prop, { writable: true });
                 }
                 if (obj.__set(prop, value)) {
                     return true;
                 } else {
-                    obj.deleteProperty(obj, 'property1');
+                    obj.deleteProperty(obj, prop);
                     return false;
                 }
             },
@@ -100,20 +165,3 @@ class MagicalObjectClass {
         });
     }
 }
-export const MagicalObject = MagicalObjectClass;
-
-/*
-export const MagicalObject = new Proxy(MagicalObjectClass, {
-    get: (obj: any, prop: PropertyKey): boolean => {
-        if (!obj.hasOwnProperty(prop)) {
-            throw new Exception('Access to undeclared static property');
-        }
-
-    },
-    set: (obj: any, prop: PropertyKey, value: any): boolean => {
-        if (!obj.hasOwnProperty(prop)) {
-            throw new Exception('Access to undeclared static property');
-        }
-    },
-});
-*/
